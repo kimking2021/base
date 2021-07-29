99d384a7790a1988f2f1e7f7755258e0c1f4b48e
kimking2020 <65654391+kimking2020@users.noreply.github.com>
2021-07-29 13:46:07 +0800

    类加载器

--- bytecode/src/main/java/priv/kimking/base/bytecode/loadclass/ClassLoaderDemo.java
+++ bytecode/src/main/java/priv/kimking/base/bytecode/loadclass/ClassLoaderDemo.java
@@ -8,23 +8,52 @@ package priv.kimking.base.bytecode.loadclass;
  */
 public class ClassLoaderDemo {
 
+    /**
+     * 启动类加载器是用本地代码实现的类加载器，
+     * 它负责将JAVA_HOME/lib下面的核心类库或-Xbootclasspath选项指定的jar包等虚拟机识别的类库加载到内存中。
+     * 由于启动类加载器涉及到虚拟机本地实现细节，开发者无法直接获取到启动类加载器的引用
+     */
+    public static void showBootstrapClassLoader() {
+        System.out.println(System.getProperty("sun.boot.class.path"));
+    }
+
+    /**
+     * 扩展类加载器是由Sun的ExtClassLoader（sun.misc.Launcher$ExtClassLoader）实现的，
+     * 它负责将JAVA_HOME /lib/ext或者由系统变量-Djava.ext.dir指定位置中的类库加载到内存中。
+     * 开发者可以直接使用标准扩展类加载器
+     */
+    public static void showExtensionClassLoader() {
+        System.out.println(System.getProperty("java.ext.dirs"));
+    }
+
+    /**
+     * 系统类加载器是由 Sun 的 AppClassLoader（sun.misc.Launcher$AppClassLoader）实现的，
+     * 它负责将用户类路径(java -classpath或-Djava.class.path变量所指的目录，
+     * 即当前类所在路径及其引用的第三方类库的路径，如第四节中的问题6所述)下的类库加载到内存中。
+     * 开发者可以直接使用系统类加载器
+     */
+    public static void showSystemClassLoader() {
+        System.out.println(System.getProperty("java.class.path"));
+    }
+
     public static void main(String... args) {
-        // bootstrap classloader  load
-//        System.out.println(System.getProperty("sun.boot.class.path"));
-
-        // extension classloader load
-//        System.out.println(System.getProperty("java.ext.dirs"));
-
         // app classloader load 加载应用程序classpath目录下的所有jar和class文件
 //        System.getProperty("java.class.path");
 //        ClassLoader.getSystemClassLoader();
 
-        ClassLoader appCL = Thread.currentThread().getContextClassLoader();
-        ClassLoader extCL = appCL.getParent();
-        ClassLoader bootCL = extCL.getParent();
+        // ClassLoader appCL = Thread.currentThread().getContextClassLoader();
+        // ClassLoader extCL = appCL.getParent();
+        // ClassLoader bootCL = extCL.getParent();
 
 
+        // 显示 启动（Bootstrap）类加载器 加载路径
+        // showBootstrapClassLoader();
 
+        // 显示 扩展（Extension）类加载器 加载路径
+        // showExtensionClassLoader();
+
+        // 显示 系统（System）类加载器
+        showSystemClassLoader();
     }
 
 }
---
+++ bytecode/src/main/java/priv/kimking/base/bytecode/loadclass/demo/NetClassLoaderSimple.java
@@ -0,0 +1,17 @@
+package priv.kimking.base.bytecode.loadclass.demo;
+
+/**
+ * <p>
+ *
+ * @author kim
+ * @date 2021/7/28
+ */
+public class NetClassLoaderSimple {
+
+    private NetClassLoaderSimple instance;
+
+    public void setNetClassLoaderSimple(Object obj) {
+        this.instance = (NetClassLoaderSimple)obj;
+    }
+
+}
