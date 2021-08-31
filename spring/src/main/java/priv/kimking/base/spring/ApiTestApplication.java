7f6ff7fa05b67c4a7b112347a830e535ac1114b3
kimking2020 <65654391+kimking2020@users.noreply.github.com>
2021-08-31 11:06:04 +0800

    [spring] whitelist middleware test

---
+++ spring/src/main/java/priv/kimking/base/spring/ApiTestApplication.java
@@ -0,0 +1,19 @@
+package priv.kimking.base.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * @author kim
+ * @date 2021/8/30
+ */
+@SpringBootApplication
+public class ApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestApplication.class, args);
+    }
+
+}
